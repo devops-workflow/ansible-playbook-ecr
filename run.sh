b56fe773980b065594f6a7f7be8d23291c4f7154
Steven Nemetz <snemetz@wiser.com>
2017-10-06 10:16:24 -0700

    Add docker version

--- run.sh
+++ run.sh
@@ -4,11 +4,14 @@
 export ANSIBLE_ROLES_PATH=$(pwd)/roles:/etc/ansible/roles
 export python=$(which python)
 
-#env | sort | grep -v ^LESS_TERMCAP
+env | sort | grep -v ^LESS_TERMCAP
 
 echo "Install: pip modules..."
 pip install -r requirements.txt
+
+echo "Versions"
 ansible --version
+docker --version
 
 echo "Install: Ansible roles..."
 ansible-galaxy install -f -r requirements.yml
@@ -18,5 +21,5 @@ ansible-playbook -i inventory playbook.yml --syntax-check --list-tasks
 echo "Test: Lint"
 ansible-lint -x ANSIBLE0012,ANSIBLE0013 playbook.yml
 
-echo "Run test playbook"
+echo "Run: playbook"
 ansible-playbook -i inventory playbook.yml --extra-vars ansible_python_interpreter=${python} --extra-vars aws_region=us-west-2 -vvvv
